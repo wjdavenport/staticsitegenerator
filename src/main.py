6640c56289ede605cd498aae3580cf3ef4f18f8c
wjdavenport <jeptha.davenport@gmail.com>
2024-05-09 10:51:19 -0600

    debug main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
-./main.sh
-# hello world
 from textnode import TextNode
 
+def main():
+    # Create a new TextNode object with dummy values
+    text_node = TextNode("This is a text node", "bold", "https://www.boot.dev")
+
+    # Print the object
+    print(text_node)
+
+# Call the main function
+main()
